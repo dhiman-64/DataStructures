0cf9a6d8eb9682f411d677552d13d32c72460641
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-03-01 14:49:21 +0530

    Second Commit

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        int a,b;
+        cin>>a>>b;
+        int c = a+b;
+        cout<<c<<endl;
+    }
+}
