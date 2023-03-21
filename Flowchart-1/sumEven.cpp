3b6544ac9065ba01e35f4f8e12b87af2eae4fd63
Ikshit Dhiman <75236509+dhiman-64@users.noreply.github.com>
2023-03-21 18:30:13 +0530

    Update sumEven.cpp

--- Flowchart-1/sumEven.cpp
+++ Flowchart-1/sumEven.cpp
@@ -7,11 +7,11 @@ int main()
     cout<<"Enter a number: ";
     cin>>n;
 
-    int i=2;
+    int i=1;
     int sum=0;
     while(i<=n)
     {
-        if(n%i==0)
+        if(i%2==0)
         {
             sum+=i;
             
