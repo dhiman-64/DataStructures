00579ed3846be9fa4d70334f29456824f6e67d6a
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-03-23 23:10:53 +0530

    Basic patterns

---
+++ Conditional and Loops-3/sumOddEven.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int sumodd=0;
+    int sumeven=0;
+
+    while(n--){
+
+    }
+}
