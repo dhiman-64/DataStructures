751a727d990ad18508110cfc3757a7d7d8a2c770
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-03-28 08:44:57 +0530

    Getting Started

---
+++ 02_Getting Started/Template.cpp
@@ -0,0 +1,7 @@
+#include<iostream>  // it is used to include the library in which the code is written.
+using namespace std; 
+
+int main() // all the basic code compiling starts form here only.
+{
+    cout<<"Hello World"<<endl;// output
+}
