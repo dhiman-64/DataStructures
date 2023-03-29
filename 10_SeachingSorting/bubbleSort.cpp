f763f0213fbe387b7ef2c73f8d521745b5ff7eae
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-03-29 19:06:51 +0530

    Bubble Sort with Optmised Approach

--- 10_SeachingSorting/bubbleSort.cpp
+++ 10_SeachingSorting/bubbleSort.cpp
@@ -4,13 +4,15 @@ using namespace std;
 void bubbleSort(int arr[], int n)
 {
     for(int i=0 ; i<n-1 ; i++){
+        bool swapped = false;
         for(int j=0 ; j<n-1-i ; j++){
             if(arr[j]>arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                swap(arr[j],arr[j+1]);
+                swapped=true;
             }
         }
+        //Already Sorted
+        if(swapped==false) break; 
     }
 }
 
