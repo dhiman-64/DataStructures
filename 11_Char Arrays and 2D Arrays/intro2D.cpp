bf7c6ad307355d1faba31a09ed1788b6420ebb7f
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-04-02 15:35:45 +0530

    Intrduction to 2D arrays

--- 11_Char Arrays and 2D Arrays/intro2D.cpp
+++ 11_Char Arrays and 2D Arrays/intro2D.cpp
@@ -11,8 +11,16 @@ int main()
             cin>>arr[i][j];
         }
     }  
+    cout<<"Print Row Wise"<<endl;
     for(int i=0; i<n; i++){
         for(int j=0; j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"Print column Wise"<<endl;
+    for(int j=0;j<m;j++){
+        for(int i=0;i<n;i++){
             cout<<arr[i][j]<<" ";
         }
         cout<<endl;
