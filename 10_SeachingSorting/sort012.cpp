82bedaf28ce574417a98b39ffe6f5272e9afb441
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-04-02 12:16:33 +0530

    Sort 012

--- 10_SeachingSorting/sort012.cpp
+++ 10_SeachingSorting/sort012.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 
 
-// 1. Brute Force -> Merge Sort
-// 2. Better Aproach -> using count of each 0,1,2
+// 1. Brute Force -> Merge Sort -> T(n)=O(nlogn)  S(n)=O(n)
+// 2. Better Approach -> using count of each 0,1,2 -> T(n)=O(2n)  S(n)=O(1);
+// 3. Optimised Approach -> Ducth National Algorithm  ->T(n)=O()  S(n)=O()
 
 void print(int arr[], int n)
 {
@@ -12,29 +13,53 @@ void print(int arr[], int n)
     }
 }
 
+// void sort012(int arr[], int n)
+// {
+//     int count0 = 0;
+//     int count1 = 0;
+//     int count2 = 0;
+
+//     for(int i=0; i<n; i++){
+//         if(arr[i]==0) count0++;
+//         else if(arr[i]==1) count1++;
+//         else count2++;
+//     }
+//     for(int i=0; i<count0; i++){
+//         arr[i]=0;
+//     }
+//     int temp = count0 + count1;
+//     for(int i=count0; i<temp; i++){
+//         arr[i]=1;
+//     }
+//     for(int i=temp; i<n; i++){
+//         arr[i]=2;
+//     }
+//     print(arr,n);
+// }
+
+
 void sort012(int arr[], int n)
 {
-    int count0 = 0;
-    int count1 = 0;
-    int count2 = 0;
-
-    for(int i=0; i<n; i++){
-        if(arr[i]==0) count0++;
-        else if(arr[i]==1) count1++;
-        else count2++;
+    int low=0;
+    int mid=0;
+    int high=n-1;
+    while(mid<=high){
+        if(arr[mid]==0){
+            swap(arr[low],arr[mid]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid]==1){
+            mid++;
+        }
+        else{
+            swap(arr[mid],arr[high]);
+            high--;
+        }
+        print(arr,n);
     }
-    for(int i=0; i<count0; i++){
-        arr[i]=0;
-    }
-    int temp = count0 + count1;
-    for(int i=count0; i<temp; i++){
-        arr[i]=1;
-    }
-    for(int i=temp; i<n; i++){
-        arr[i]=2;
-    }
-    print(arr,n);
 }
+
 
 int main()
 {
