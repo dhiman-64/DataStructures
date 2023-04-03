9e25f578fbc66a39249b219ed6c0ffac167ddbda
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-04-03 18:47:03 +0530

    Sort 012 using Dutch National Flag Algorithm

--- Striver SDE Sheet/sort012.cpp
+++ Striver SDE Sheet/sort012.cpp
@@ -1,40 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Brute Force -> T(N)=O(NlogN)   S(N) = O(N)
 
-// 1. Brute Force -> Merge Sort
-// 2. Better Aproach -> using count of each 0,1,2
-// 3. Optimised Approach -> Dutch National Algoritms
+// Better Approach -> T(n) = O(n+n)  S(N) = O(1)
 
-void print(int arr[], int n)
-{
-    for(int i=0; i<n; i++){
-        cout<<arr[i]<<" ";
-    }
-}
+// void sort012(int arr[], int n)
+// {
+//     int count0 = 0;
+//     int count1 = 0;
+//     int count2 = 0;
+//     for(int i=0 ; i<n ; i++){
+//         if(arr[i]==0) count0++;
+//         if(arr[i]==1) count1++;
+//         if(arr[i]==2) count2++;
+//     }
+
+//     for(int i=0 ; i<count0 ; i++){
+//         arr[i] = 0;
+//     }
+//     for(int i= count0 ; i<count0+count1 ; i++){
+//         arr[i] = 1;
+//     }
+//     for(int i=count0+count1 ; i<n ; i++){
+//         arr[i] = 2;
+//     }
+// }
+
+
+// Optimised Approach  -> T(N) = O(N)  S(N) = O(1)
 
 void sort012(int arr[], int n)
 {
-    int count0 = 0;
-    int count1 = 0;
-    int count2 = 0;
-
-    for(int i=0; i<n; i++){
-        if(arr[i]==0) count0++;
-        else if(arr[i]==1) count1++;
-        else count2++;
+    int low = 0;
+    int mid = 0;
+    int high = n-1;
+    
+    while(mid<=high)
+    {
+        if(arr[mid]==0){
+            swap(arr[low],arr[mid]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid]==1){
+            mid++;
+        }
+        else if(arr[mid]==2){
+            swap(arr[mid],arr[high]);
+            high--;
+        }
     }
-    for(int i=0; i<count0; i++){
-        arr[i]=0;
-    }
-    int temp = count0 + count1;
-    for(int i=count0; i<temp; i++){
-        arr[i]=1;
-    }
-    for(int i=temp; i<n; i++){
-        arr[i]=2;
-    }
-    print(arr,n);
 }
 
 int main()
@@ -42,10 +58,13 @@ int main()
     int n;
     cin>>n;
     int arr[n];
-    for(int i=0; i<n; i++)
-    {
+    for(int i=0 ; i<n ; i++){
         cin>>arr[i];
     }
-    // Function Calling
+    //Function Call
     sort012(arr,n);
+    for(int i=0 ; i<n ; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
 }
