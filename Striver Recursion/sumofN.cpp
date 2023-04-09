45b2cd24a296300dd270b02e190f24a68a4f4fd5
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-04-09 22:33:12 +0530

    Allocating2DDynamicArray

---
+++ Striver Recursion/sumofN.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int sumPrint(int n)
+{
+    if(n==0){
+        return;
+    }
+    int sum = 
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    //Function Calling
+    cout<<sumPrint(n);
+}
