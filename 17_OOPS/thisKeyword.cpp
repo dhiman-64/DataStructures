0a6bfd81f6d8e2b5f229efef492dbf393c9289d0
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-04-11 07:16:32 +0530

    this Keyword - pointer variable

---
+++ 17_OOPS/thisKeyword.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+#include "createClass4.cpp"
+
+// this-> act as pointer variable
+
+int main()
+{
+    Student s1(10,1001);
+    cout<<"Address of s1: "<<&s1<<endl;
+}
