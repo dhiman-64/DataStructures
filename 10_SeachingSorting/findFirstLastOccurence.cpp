686908b45ad15f6570b1ee7fe76b15464a6dc241
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-04-12 18:02:53 +0530

    Find First and Last element

--- 10_SeachingSorting/findFirstLastOccurence.cpp
+++ 10_SeachingSorting/findFirstLastOccurence.cpp
@@ -1,39 +1,18 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-int firstOcc(int arr[], int n, int k)
-{
-    int start = 0;
-    int end = n-1;
-    int store = -1;
-    while(start <=  end){
-        int mid = start + (end-start)/2;
-        if(arr[mid]==k){
-            store = mid;
-            end = mid-1;
-        }
-        else if(arr[mid] > k){
-            end = mid-1;
-        }
-        else{
-            start = mid+1;
-        }
-    }
-    return store;
-}
-
-int lastOcc(int arr[], int n, int k)
+int lastOcc(int arr[], int n, int target)
 {
     int start = 0;
     int end = n-1;
     int store = -1;
     while(start <= end){
         int mid = start + (end-start)/2;
-        if(arr[mid]==k){
+        if(arr[mid]==target){
             store = mid;
             start = mid+1;
         }
-        else if(arr[mid] > k){
+        else if(arr[mid] > target){
             end = mid-1;
         }
         else{
@@ -43,28 +22,39 @@ int lastOcc(int arr[], int n, int k)
     return store;
 }
 
-pair<int,int>firstAndLastPosition(int arr[], int n, int k)
+int firstOcc(int arr[], int n, int target)
 {
-    pair<int,int> p;
-    p.first = firstOcc(arr,n,k);
-    p.second = lastOcc(arr,n,k);
-    return p;
+    int start = 0;
+    int end = n-1;
+    int store = -1;
+    while(start <= end){
+        int mid = start +(end-start)/2;
+
+        if(arr[mid]==target){
+            store = mid;
+            end = mid-1;
+        }
+        else if(arr[mid] > target){
+            end = mid-1;
+        }
+        else if(arr[mid] < target){
+            start = mid+1;
+        }
+    }
+    return store;
 }
 
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        int arr[n];
-        for(int i=0 ; i<n ; i++){
-            cin>>arr[i];
-        }
-        int k;
-        cin>>k;
-        //Function Calling
-        firstAndLastPosition(arr,n,k);
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0 ; i<n ; i++){
+        cin>>arr[i];
     }
+    int target;
+    cin>>target;
+    //Function Calling
+    cout<<firstOcc(arr,n,target)<<" "<<lastOcc(arr,n,target)<<endl;
+
 }
