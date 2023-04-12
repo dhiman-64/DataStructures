2ab20506e0b514197a256b09b72706b4e3e1961c
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-04-12 07:04:27 +0530

    Constant Function ( const )

---
+++ 18_OOPS/useConstant.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+#include "ConstantFuction.cpp"
+
+int main()
+{
+    Fraction f1(10,2);
+    Fraction f2(15,4);
+
+    Fraction const f3;
+    
+    cout<<f3.getNumerator()<<" "<<f3.getDenominator()<<endl;
+
+    // f3.setNumerator(10);
+
+}
