1913c1db7286e5bdcab7d6dda4efed1d67938a47
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-04-27 15:05:07 +0530

    Day 1

---
+++ Striver SDE Sheet/Day-1/BuyandSell.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProfit(int prices[], int n)
+{
+    int minPrice = INT_MAX;
+    int profit = 0;
+    for(int i=0 ; i<n ; i++)
+    {
+        minPrice = min(minPrice , prices[i]);
+        profit = max(profit , prices[i]-minPrice);
+    }
+    return profit;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int prices[n];
+    for(int i=0 ; i<n ; i++)
+    {
+        cin>>prices[i];
+    }
+    //Function Calling
+    cout<<maxProfit(prices,n)<<endl;
+}
