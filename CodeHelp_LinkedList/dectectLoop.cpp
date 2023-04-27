34ad2c1ced9e55f251e000cac31fadd92405e38d
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-04-27 06:35:53 +0530

    Dectect Loops : Return true/false

---
+++ CodeHelp_LinkedList/dectectLoop.cpp
@@ -0,0 +1,118 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+void print(Node* head)
+{
+    Node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+void insertAtHead(Node* &head, Node* &tail, int data)
+{
+    if(head == NULL){
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+    }
+    else{
+        Node* newNode = new Node(data);
+        newNode->next = head;
+        head = newNode;
+    }
+}
+
+void insertAtTail(Node* &head, Node* &tail, int data)
+{
+    if(tail == NULL){
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+    }
+    else{
+        Node* newNode = new Node(data);
+        tail->next = newNode;
+        tail = tail->next;
+    }
+}
+
+void insertAtMiddle(Node* &head, Node* &tail, int position, int data)
+{
+    if(position == 1){
+        insertAtHead(head, tail, data);
+        return;
+    }
+    // Traverse
+    int count = 1;
+    Node* temp = head;
+    while(count < position-1){
+        temp = temp->next;
+        count++;
+    }
+
+    if(temp->next == NULL){
+        insertAtTail(head, tail, data);
+        return;
+    }
+    Node* newNode = new Node(data);
+    newNode->next = temp->next;
+    head->next = newNode;
+}
+
+
+bool detectLoops(Node* head)
+{
+    if(head== NULL){
+        return false;
+    }
+    map<Node*, bool> visited;
+    Node* temp = head;
+
+    while(temp != NULL){
+
+        // Cycle is present
+        if(visited[temp] == true){
+            return true;
+        }
+
+        visited[temp] = true;
+        temp = temp->next;
+    }
+    return false;
+}
+
+int main()
+{
+    Node* n1 = new Node(10);
+    Node* head = n1;
+    Node* tail = n1;
+
+    insertAtTail(head, tail, 20);
+    insertAtTail(head, tail, 30);
+    insertAtTail(head, tail, 40);
+    insertAtMiddle(head, tail, 5, 50);
+
+    tail->next = head->next;
+
+    if(detectLoops(head)){
+        cout<<"Cycle is present"<<endl;
+    }
+    else{
+        cout<<"Cycle is not Present"<<endl;
+    }
+}
