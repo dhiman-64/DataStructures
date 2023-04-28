2f3633a240e9fdc27aa087fad53b730563d82a66
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-04-28 12:41:20 +0530

    Two Sum

---
+++ Striver SDE Sheet/Day-4/twoSum.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector<int> twoSum(int arr[], int n, int target)
+{
+    map<int ,int > mpp;
+    for(int i=0 ; i<n ; i++)
+    {
+        int x = arr[i];
+        int other = target - x;
+        if(mpp.find(other) != mpp.end()){
+            return {mpp[other] , i};
+        }
+        mpp[x] = i;
+    }
+    return{-1,-1};
+}
+
+
+
+// Brute Force
+/*
+
+T(N) = O(N^2)
+S(N) = O(1)
+
+vector<int> twoSum(int arr[], int n, int target)
+{
+    for(int i=0 ; i<n ; i++){
+        for(int j=i+1 ; j<n ; j++){
+            if(arr[i] + arr[j] == target){
+                return {i,j};
+            }
+        }
+    }
+    return {-1,-1};
+}
+*/
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0 ; i<n ; i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin>>target;
+    // Function Calling
+    vector<int> ans = twoSum(arr,n,target);
+    cout<<ans[0]<<" "<<ans[1]<<endl;
+}
