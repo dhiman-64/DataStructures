388c52e7720e438b7bdff6f37bdc87f443691383
Ikshit Dhiman <dhimanikshit64@gmail.com>
2023-05-15 22:32:06 +0530

    Revision

---
+++ 20_Stack Queues/00_Practice.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<climits>
+
+using namespace std;
+
+class Stack{
+    int *data;
+    int nextIndex;
+    int capacity;
+
+    public:
+    Stack(int totalSize){
+        data = new int[totalSize];
+        nextIndex = 0;
+        capacity = totalSize;    
+    }
+
+    // 1. SIZE
+    int getSize(){
+        return nextIndex;
+    }
+
+    // 2. Check if Empty
+    bool isEmpty(){
+        if(nextIndex == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    // 3. Push -> insertion
+    void push(int element){
+        if(nextIndex == capacity){
+            cout<<"Stack is Full"<<endl;
+            return;
+        }
+        data[nextIndex] = element;
+        nextIndex++;
+    }
+
+    // 4. Pop -> deletion
+    int pop(){
+        if(isEmpty()){
+            cout<<"Stack is Empty"<<endl;
+            return INT_MIN;
+        }
+        nextIndex--;
+        return data[nextIndex];
+    }
+
+    // 5. Read uppermost element -> TOP()
+    int top(){
+        if(isEmpty()){
+            cout<<"Stack is Empty"<<endl;
+            return INT_MIN;
+        }
+        return data[nextIndex - 1];
+    }
+};
+
+int main()
+{
+    Stack s1(4);
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+    s1.push(50);
+    cout<<s1.getSize()<<endl;
+    cout<<s1.top()<<endl;
+    cout<<s1.pop()<<endl;
+
+    cout<<s1.getSize()<<endl;
+    cout<<s1.top()<<endl;
+    cout<<s1.pop()<<endl;
+
+    cout<<s1.getSize()<<endl;
+    cout<<s1.top()<<endl;
+    cout<<s1.pop()<<endl;
+}
